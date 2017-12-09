972631523da16f4582d6871677e9151293a1ac00
chang8128 <chang.zhiping@qq.com>
2017-12-09 21:14:37 +0800

    在Ubuntu V16 X64 上定期更新Choice A股历史行情数据
    "2011-10-01", "2011-10-31",

---
+++ choice_py_ub_2.x/api_csd_history.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# encoding: utf-8
+import MySQLdb
+
+import pandas as pd
+from sqlalchemy import create_engine  
+# pandas.set_option('display.max_rows',None)    
+
+# 检索股票代码
+db = MySQLdb.connect("localhost","fmtc","FMTC@2017","fmtcdb" )
+cursor = db.cursor()
+sql1 = """select CODES from t_sector_20121231"""
+a = cursor.execute(sql1)
+codes = cursor.fetchmany(a)
+
+# 数一数有多少个列表值
+numrows = int(cursor.rowcount)
+print numrows
+
+# for i in range(0,len(codes)):
+# 	print list(codes[i])[0]
+    
+db.close()
+
+
+__author__ = 'Administrator'
+
+from EmQuantAPI import *
+
+#调用登录函数（激活后使用，不需要用户名密码）
+loginResult = c.start("ForceLogin=0")
+
+if(loginResult.ErrorCode != 0):
+    print "login in fail"    
+    
+
+# csd使用范例，有用：序列函数，获取股票，指数，基金，等证券品种的，日频历史行情序列数据，
+data = c.csd(codes, "open,close,high,low,preclose,AVERAGE,CHANGE,PCTCHANGE,VOLUME,HIGHLIMIT,AMOUNT,TURN,TRADESTATUS,LOWLIMIT,AMPLITUDE,TNUM,TAFACTOR,FRONTTAFACTOR,ISSTSTOCK,ISXSTSTOCK", "2011-10-01", "2011-10-31", "RowIndex=1,period=1,adjustflag=1,curtype=1,pricetype=1,year=2011,Ispandas=1") 
+
+# 显示DataFrame 中的数据
+# print data
+
+# 调用sqlalchemy 方法将DataFrame 中的数据写入数据库中。
+connect_fmtc = create_engine('mysql+mysqldb://fmtc:FMTC@2017@localhost:3306/fmtcdb?charset=utf8')  
+pd.io.sql.to_sql(data,'t_csd_history', connect_fmtc, schema='fmtcdb', if_exists='append')  
+
+
